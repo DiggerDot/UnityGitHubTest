fb34a6e6bd9063c08fb754f3a0f59437476aac25
DiggerDot <itsabeau2fulday@gmail.com>
2018-12-29 17:47:38 +0200

    Create speak module frame
    Create frame.
    Add fun Speak()
    Add fun Repeat

---
+++ Assets/Game Core/SpeakeModule/Speaker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speaker : MonoBehaviour {
+
+    /// <summary>
+    /// Произносимы dialog
+    /// </summary>
+    string txt;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// The character makes the speech.
+    /// </summary>
+    /// <param name="speech"></param>
+    public void Speak(string speech)
+    {
+        txt = speech;
+        Debug.Log("Speech: " + txt);
+    }
+
+    /// <summary>
+    /// Speech weel repesat
+    /// </summary>
+    public void Repeat()
+    {
+        Debug.Log("Speech: " + txt);
+    }
+
+
+}
