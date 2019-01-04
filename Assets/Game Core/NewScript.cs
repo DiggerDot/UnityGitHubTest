4af1e77da7e510b2a2befd67caf977d9209e9681
DiggerDot <itsabeau2fulday@gmail.com>
2019-01-04 11:20:02 +0200

    From another branch

---
+++ Assets/Game Core/NewScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Test by name conflict");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
